06f52bc35ffd6921a533bf7219128d0b1c81bac3
Sarthak Uniyal <uniyalsarthak115@gmail.com>
2025-11-17 09:03:04 +0530

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai 
-import os 
-genai.configure(api_key="2a2lVUoaHek89gjRdRrWMLXEGmxBLo8") 
-try: 
-    model = genai.GenerativeModel("models/gemini-2.5-flash")
-    response = model.generate_content("test") 
-    print("PAID Gemini key is working!") 
-except Exception as e: print(str(e))
